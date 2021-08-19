05ba52c85cc810dadd931941db2d5768eaa688cb
Nguyễn Đạt <69216709+nguyenhuudat123@users.noreply.github.com>
2021-08-19 21:45:05 +0900

    Update bt09.py

--- lesson09/bt09.py
+++ lesson09/bt09.py
@@ -1,4 +1,12 @@
 #sinh ra dict moi tu list cac dict
 lst_dict = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
-dict_ = {i['item']: i['amount'] for i in lst_dict}
-print(dict_)
+output = {}
+for element in lst_dict:
+  item = element['item']
+  amount = element['amount']
+  if item not in output:
+    output[item] = amount
+   else:
+    output[item] += amount
+   
+print(output)
